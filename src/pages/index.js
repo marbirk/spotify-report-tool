1322747f902f60bfd157e335490d8b912456c261
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-10 17:35:34 +0100

    Add text color to global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 export default function Home() {
     return (
         <Layout>
-            <div className="text-gray-800 dark:text-gray-200">Hello world!</div>
+            <div>Hello world!</div>
         </Layout>
     )
 }
