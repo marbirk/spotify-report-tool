6f678bce235519d5b71ef9b8964f38015d74faea
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-10 13:35:20 +0100

    Add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './layout.css'
 import Header from './Header'
+import Footer from './Footer'
 
 export default function Layout({ children }) {
     return (
         <div>
             <Header />
             {children}
-            <footer></footer>
+            <Footer />
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import DarkModeToggle from '../components/DarkModeToggle'
 
 export default function Home() {
     return (
