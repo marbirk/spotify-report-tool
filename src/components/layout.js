680fc1e2a617bdbade6245b8966a8521ef70a665
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-10 14:34:16 +0100

    Add main container

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,10 +5,10 @@ import Footer from './Footer'
 
 export default function Layout({ children }) {
     return (
-        <div className="container mx-auto">
+        <main className="container mx-auto">
             <Header />
             {children}
             <Footer />
-        </div>
+        </main>
     )
 }
