73c7b3234d32ad573facce9cc0d166a33b957bc9
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-12 11:26:34 +0100

    Adjust profile text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,12 @@ export default function Home() {
     return (
         <Layout>
             <div>
-                Frontend Engineer, Working for Steinberg, Master student, last
-                project?, study focus?, network?
+                Frontend Engineer, Working for Steinberg, Master student at TH
+                Lübeck, last project: Personas in e-commerce - evaluation of the
+                data-driven persona method as a basis for product
+                recommendations in online retail for customers with musical
+                interest, study focus: Modern and clean software development &
+                Data Science , network?
             </div>
         </Layout>
     )
