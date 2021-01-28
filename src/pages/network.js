e8a78e62e8664368fbd517a7aa6bd8a3422bffc2
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-28 22:20:10 +0100

    Add contacts to network page

--- src/pages/network.js
+++ src/pages/network.js
@@ -2,11 +2,63 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 const NetworkPage = () => {
+    const networkList = [
+        {
+            name: 'Dragan Vidovic',
+            web: 'http://www.draganvidovic.com/',
+        },
+        {
+            name: 'Jone Schardt',
+            web: 'https://joneschardt.de/',
+        },
+        {
+            name: 'Nils Wiere',
+            web: 'https://nilswiere.de/',
+        },
+        {
+            name: 'Andreas Rissling',
+            web: 'https://www.gueteklasse-a.de/',
+        },
+        {
+            name: 'Lars Graubner',
+            web: 'https://larsgraubner.com/',
+        },
+        {
+            name: 'Institut für persönliche Bildung',
+            web: 'https://ifpb.eu/',
+        },
+        {
+            name: 'Glücklich in 90 Minuten',
+            web: 'https://www.gluecklich-in-90-minuten.com/',
+        },
+        {
+            name: 'Bianca Gibisch',
+            web: 'https://biancagibisch.de/',
+        },
+        {
+            name: 'Naturheilpraxis Ingrid Berger',
+            web: 'http://naturheilpraxis-ingrid-berger.de/',
+        },
+    ]
     return (
         <Layout>
             <h1>Network</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {renderNetworkList()}
+            </div>
         </Layout>
     )
+
+    function renderNetworkList() {
+        return networkList.map(contact => {
+            return (
+                <a href={contact.web} className="w-full h-32">
+                    Initialien?
+                    {contact.name}
+                </a>
+            )
+        })
+    }
 }
 
 export default NetworkPage
