f6084f2981ccdf23f47a05f0df872bfdbdcdeb4f
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-28 17:42:15 +0100

    Refactor typewriter functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Layout from '../components/Layout'
 import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 import Img from 'gatsby-image'
+import Typewriter from 'typewriter-effect'
 
 const HomePage = ({ data }) => {
     const { title, description, keywords } = data.site.siteMetadata
     const topFiveArtists = data.allSpotifyTopArtist.edges
-    const headlineStrings = [
-        'Marcel Birkhahn',
-        'Frontend Engineer (B.Sc.)',
-        'Student of media informatics (M.Sc.)',
-        'Rock music enthusiast',
-        'Festival fan',
-    ]
-    useEffect(() => {
-        startTextAnimation(0)
-    }, [])
     return (
         <Layout>
             <Helmet
@@ -34,19 +25,32 @@ const HomePage = ({ data }) => {
             <section>
                 <p className="text-5xl uppercase">I am</p>
                 <h1 className="text-5xl uppercase squishy-text">
-                    Marcel Birkhahn
+                    <Typewriter
+                        options={{
+                            strings: [
+                                'Marcel Birkhahn',
+                                'Frontend Engineer',
+                                'Informatics Student',
+                                'Hardrock enthusiast',
+                                'Festival fan',
+                            ],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
                 </h1>
             </section>
             <section>
                 <p>
                     I'm working for Steinberg Media Technologies GmbH as a
-                    Frontend Engineer in Hamburg.
+                    Frontend Engineer (B.Sc.) in Hamburg.
                 </p>
             </section>
             <section>
                 <p>
-                    Master student at TH Lübeck, study focus: Modern and clean
-                    software development & Data Science
+                    I'm doing my studies of media informatics (M.Sc.) at TH
+                    Lübeck, study focus: Modern and clean software development &
+                    Data Science
                 </p>
                 <p>
                     My last project: Personas in e-commerce - evaluation of the
@@ -62,42 +66,6 @@ const HomePage = ({ data }) => {
             network page?, Storybook?, Unit tests?
         </Layout>
     )
-
-    function startTextAnimation(i) {
-        if (typeof headlineStrings[i] === 'undefined') {
-            setTimeout(function () {
-                startTextAnimation(0)
-            }, 20000)
-        }
-        console.log(headlineStrings[i])
-        // check if dataText[i] exists
-        if (i < headlineStrings[i].length) {
-            // text exists! start typewriter animation
-            typeWriter(headlineStrings[i], 0, function () {
-                // after callback (and whole text has been animated), start next text
-                startTextAnimation(i + 1)
-            })
-        }
-    }
-
-    function typeWriter(text, i, fnCallback) {
-        // chekc if text isn't finished yet
-        if (i < text.length) {
-            // add next character to h1
-            document.querySelector('h1').innerHTML =
-                text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
-
-            // wait for a while and call this function again for next character
-            setTimeout(function () {
-                typeWriter(text, i + 1, fnCallback)
-            }, 100)
-        }
-        // text finished, call callback if there is a callback function
-        else if (typeof fnCallback == 'function') {
-            // call callback after timeout
-            setTimeout(fnCallback, 700)
-        }
-    }
 
     function renderBandList() {
         return topFiveArtists.map(artist => {
