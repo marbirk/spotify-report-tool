a66c0f0be4759c7b35def4919342b7f5c42ac409
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-30 16:18:35 +0100

    Add home link to headline banner

--- src/components/HeadlineBanner.js
+++ src/components/HeadlineBanner.js
@@ -1,29 +1,32 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect'
+import { Link } from 'gatsby'
 
 const HeadlineBanner = props => {
     const isStartPage = props.pathname.length === 1
     return (
         <section className="mb-12">
-            <h1 className="text-5xl uppercase squishy-text">
-                {isStartPage ? (
-                    <Typewriter
-                        options={{
-                            strings: [
-                                'I am Marcel Birkhahn',
-                                'Frontend Engineer',
-                                'Informatics Student',
-                                'Hard Rock Enthusiast',
-                                'Festival Fan',
-                            ],
-                            autoStart: true,
-                            loop: true,
-                        }}
-                    />
-                ) : (
-                    'Marcel Birkhahn'
-                )}
-            </h1>
+            <Link to="/">
+                <h1 className="text-6xl uppercase squishy-text">
+                    {isStartPage ? (
+                        <Typewriter
+                            options={{
+                                strings: [
+                                    'I am Marcel Birkhahn',
+                                    'Frontend Engineer',
+                                    'Informatics Student',
+                                    'Hard Rock Enthusiast',
+                                    'Festival Fan',
+                                ],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    ) : (
+                        'Marcel Birkhahn'
+                    )}
+                </h1>
+            </Link>
         </section>
     )
 }
