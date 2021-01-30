cd9d081d8c03edaff9223d420702f2ddcf596fd6
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-30 19:48:25 +0100

    Refactor headline banner

--- src/components/HeadlineBanner.js
+++ src/components/HeadlineBanner.js
@@ -5,29 +5,43 @@ import { Link } from 'gatsby'
 const HeadlineBanner = props => {
     const isStartPage = props.pathname.length === 1
     return (
-        <section className="mb-12">
+        <section className="mb-12 mt-6">
             <Link to="/">
-                <h1 className="text-6xl uppercase squishy-text">
-                    {isStartPage ? (
-                        <Typewriter
-                            options={{
-                                strings: [
-                                    'I am Marcel Birkhahn',
-                                    'Frontend Engineer',
-                                    'Informatics Student',
-                                    'Hard Rock Enthusiast',
-                                    'Festival Fan',
-                                ],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
-                    ) : (
-                        'Marcel Birkhahn'
-                    )}
+                <h1 className="text-5xl uppercase squishy-text">
+                    {isStartPage ? getStartPageBanner() : getDefaultBanner()}
                 </h1>
             </Link>
         </section>
+    )
+}
+
+const getStartPageBanner = () => {
+    return (
+        <>
+            <span>I am</span>
+            <Typewriter
+                options={{
+                    strings: [
+                        'Marcel Birkhahn',
+                        'Frontend Engineer',
+                        'Informatics Student',
+                        'Hard Rock Enthusiast',
+                        'Festival Fan',
+                    ],
+                    autoStart: true,
+                    loop: true,
+                }}
+            />
+        </>
+    )
+}
+
+const getDefaultBanner = () => {
+    return (
+        <>
+            <span className="block">Marcel</span>
+            <span className="block">Birkhahn</span>
+        </>
     )
 }
 
