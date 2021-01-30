5767aa53be3698e79195c7bb90472216d746e63a
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-01-30 22:11:34 +0100

    Add greensta logo

--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -5,6 +5,12 @@ const ImprintPage = ({ location }) => {
     return (
         <Layout location={location}>
             <h2>Imprint</h2>
+            <img
+                src="https://ssl.greensta.de/wp-content/uploads/2019/12/100prozent_banner-250-white.png"
+                width="250"
+                height="110"
+                alt="Greensta Logo"
+            />
         </Layout>
     )
 }
