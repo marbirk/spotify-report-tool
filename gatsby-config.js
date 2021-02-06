9bd195c13a893d6245440250a437f7025b69cffb
Marcel Birkhahn <m.birkhahn@steinberg.de>
2021-02-06 13:56:57 +0100

    Change google analytics to gdpr variant

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,32 +41,23 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-plugin-google-analytics`,
+            resolve: `gatsby-plugin-google-analytics-gdpr`,
             options: {
-                // The property ID; the tracking code won't be generated without it
+                // The property ID; the tracking code won't be generated without it.
                 trackingId: 'UA-47721885-1',
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: false,
-                // Setting this parameter is optional
-                anonymize: true,
-                // Setting this parameter is also optional
-                respectDNT: true,
-                // Avoids sending pageview hits from custom paths
-                exclude: ['/preview/**', '/do-not-track/me/too/'],
-                // Delays sending pageview hits on route update (in milliseconds)
-                pageTransitionDelay: 0,
-                // Enables Google Optimize using your container Id
-                optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-                // Enables Google Optimize Experiment ID
-                experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-                // Set Variation ID. 0 for original 1,2,3....
-                variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-                // Defers execution of google analytics script after page load
-                defer: false,
-                // Any additional optional fields
-                sampleRate: 5,
-                siteSpeedSampleRate: 10,
-                cookieDomain: 'example.com',
+                // Optional parameter (default false) - Enable analytics in development mode.
+                enableDevelopment: true, // default false
+                // Optional parameter (default true) - Some countries (such as Germany) require you to use the _anonymizeIP function for Google Analytics. Otherwise you are not allowed to use it.
+                anonymizeIP: true,
+                // Optional parameter (default false) - Starts google analytics with cookies enabled. In some countries (such as Germany) this is not allowed.
+                autoStartWithCookiesEnabled: false,
+                // Optional parameter - Configuration for react-ga and google analytics
+                reactGaOptions: {
+                    debug: true,
+                    gaOptions: {
+                        sampleRate: 10,
+                    },
+                },
             },
         },
     ],
