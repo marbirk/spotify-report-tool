0166b1d6b079758355a797aab300622326af392c
Marcel Birkhahn <m.birkhahn@posteo.de>
2021-02-14 21:49:46 +0100

    Adjust grid behaviour

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -6,7 +6,7 @@ interface GridProps {
 
 const Grid = ({ children, ...props }: GridProps) => (
     <div
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"
+        className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4"
         {...props}
     >
         {children}
