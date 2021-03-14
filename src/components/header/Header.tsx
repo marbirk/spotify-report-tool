f9f4cd5ac705c4f035126501a0df397a8be63aac
Marcel Birkhahn <m.birkhahn@posteo.de>
2021-03-14 17:56:00 +0100

    Fix location issue in build process

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import DarkModeToggle from '../darkModeToggle/DarkModeToggle'
 import HeadlineBanner from '../headlineBanner/HeadlineBanner'
 
 export default function Header() {
+    const [pathname, setPathname] = useState('/')
+    useEffect(() => {
+        setPathname(location.pathname)
+    }, [])
     return (
         <header className="flex flex-col" data-testid="header">
             <DarkModeToggle />
-            <HeadlineBanner pathname={location.pathname} />
+            <HeadlineBanner pathname={pathname} />
         </header>
     )
 }
