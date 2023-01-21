d8d11d5e2542198dbd2e415aa3cba3f8eb69a14a
Marcel Birkhahn <m.birkhahn@posteo.de>
2023-01-21 22:51:39 +0100

    Access link component by role

--- src/components/link/Link.test.tsx
+++ src/components/link/Link.test.tsx
@@ -6,14 +6,14 @@ import Link from './Link'
 describe('Link', () => {
     test('renders correctly gatsby link', () => {
         render(<Link to="/network">Internal Link</Link>)
-        const component = screen.getByText('Internal Link')
-        expect(component.getAttribute('target')).toBe(null)
-        expect(component).toMatchSnapshot()
+        const link = screen.getByRole('link')
+        expect(link.getAttribute('target')).toBe(null)
+        expect(link).toMatchSnapshot()
     })
     test('renders correctly external link', () => {
         render(<Link to={'https://test-url.de'}>External Link</Link>)
-        const component = screen.getByText('External Link')
-        expect(component.getAttribute('target')).toBe('_blank')
-        expect(component).toMatchSnapshot()
+        const link = screen.getByRole('link')
+        expect(link.getAttribute('target')).toBe('_blank')
+        expect(link).toMatchSnapshot()
     })
 })
