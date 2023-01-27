51a4fa9d0ebb01047855c878ca621b02ffa0f438
Marcel Birkhahn <m.birkhahn@posteo.de>
2023-01-27 09:30:05 +0100

    Fetch shows for artists

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,9 @@ module.exports = {
         `gatsby-plugin-use-dark-mode`,
         `gatsby-plugin-postcss`,
         `gatsby-plugin-react-helmet`,
-        `gatsby-transformer-sharp`,
+        `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
         `gatsby-transformer-json`,
         `gatsby-plugin-sitemap`,
         {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Grid from '../components/grid/Grid'
 import Seo from '../components/Seo'
 import Link from '../components/link/Link'
 import { useEffect } from 'react'
+import { useState } from 'react'
 
 interface HomePageProps extends PageProps {
     data: {
@@ -63,19 +64,36 @@ type SpotifyTopTrackProps = {
 }
 
 const HomePage = (props: HomePageProps) => {
+    const [shows, setShows] = useState([])
     const { title, description } = props.data.site.siteMetadata
     const tracks = props.data.allSpotifyTopTrack.edges
     const artists = props.data.allSpotifyTopArtist.edges
 
     useEffect(() => {
+        const eventsUrl = 'https://api.songkick.com/api/3.0/events.json'
         const apiKey = process.env.SONGKICK_API_KEY
-        const fetchUrl = `https://api.songkick.com/api/3.0/events.json?apikey=${apiKey}&location=geo:53.55,10.0&artist_name=mono-%26-nikitaman`
-        fetch(fetchUrl)
-            .then(r =>
-                r.json().then(data => ({ status: r.status, body: data }))
-            )
-            .then(obj => console.log(obj))
-    })
+        const location = 'geo:53.55,10.0'
+
+        artists.map(artist => {
+            const artistName = artist.node.name.toLowerCase().replace(/ /g, '-')
+            const encodedArtistName = encodeURIComponent(artistName)
+
+            const fetchUrl = `${eventsUrl}?apikey=${apiKey}&location=${location}&artist_name=${encodedArtistName}`
+            fetch(fetchUrl)
+                .then(r =>
+                    r.json().then(data => ({ status: r.status, body: data }))
+                )
+                .then(obj => {
+                    if (obj.body.resultsPage.totalEntries > 0) {
+                        setShows(
+                            obj.body.resultsPage.results.event[0].displayName
+                        )
+                    }
+                })
+        })
+    }, [])
+
+    console.log(shows)
 
     return (
         <Layout>
@@ -135,6 +153,9 @@ const renderList = (
                             ? item.node.artists[0].name
                             : item.node.genres[0]}
                     </p>
+                </div>
+                <div className="mt-4">
+                    <p>Shows around me:</p>
                 </div>
             </Link>
         )
